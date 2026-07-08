f9458eced7ee6a0ecf6d4ed9e8f8c34a6f553429
ikenna <agugbuenzubechi@gmail.com>
2026-07-08 22:02:15 +0100

    fixed cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,11 @@ app.use(helmet());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://vendorville.vercel.app/"],
+    origin: [
+      "http://localhost:3000",
+       "https://vendorville.vercel.app/",
+       "https://vendorville-backend.onrender.com/api/auth",
+      ],
     credentials: true,
   }),
 );
