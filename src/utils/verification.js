401e4364ca8cd8e51ec9208d3bc3f8b98892e952
ikenna <agugbuenzubechi@gmail.com>
2026-07-09 20:10:41 +0100

    fixed src/utils/verification.js

--- src/utils/verification.js
+++ src/utils/verification.js
@@ -12,7 +12,9 @@ const __dirname = path.dirname(__filename);
 
 // Reusable transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
