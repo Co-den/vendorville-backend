8e456989065c825c14d5bb5af1db8feb6d689793
ikenna <agugbuenzubechi@gmail.com>
2026-07-13 19:09:15 +0100

    updated businessService

--- src/utils/uploadToCloudinary.js
+++ src/utils/uploadToCloudinary.js
@@ -1,4 +1,4 @@
-import cloudinary from "#config/cloudinary.js";
+/*import cloudinary from "#config/cloudinary.js";
 
 export const uploadBufferToCloudinary = (buffer, folder) => {
   return new Promise((resolve, reject) => {
@@ -9,6 +9,30 @@ export const uploadBufferToCloudinary = (buffer, folder) => {
         resolve(result.secure_url);
       },
     );
+    stream.end(buffer);
+  });
+};*/
+
+import cloudinary from "#config/cloudinary.js";
+
+export const uploadBufferToCloudinary = (buffer, folder) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        folder,
+        resource_type: "image",
+      },
+      (error, result) => {
+        if (error) {
+          console.error("Cloudinary upload error:");
+          console.error(JSON.stringify(error, null, 2));
+          return reject(error);
+        }
+
+        resolve(result.secure_url);
+      },
+    );
+
     stream.end(buffer);
   });
 };
