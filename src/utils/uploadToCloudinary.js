21ef101f70b1294a6715cfbe36e5609a7b96a208
ikenna <agugbuenzubechi@gmail.com>
2026-07-14 10:45:30 +0100

    fixed cloudinary uploaad issues

--- src/utils/uploadToCloudinary.js
+++ src/utils/uploadToCloudinary.js
@@ -1,38 +1,16 @@
-/*import cloudinary from "#config/cloudinary.js";
-
-export const uploadBufferToCloudinary = (buffer, folder) => {
-  return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      { folder, resource_type: "image" },
-      (error, result) => {
-        if (error) return reject(error);
-        resolve(result.secure_url);
-      },
-    );
-    stream.end(buffer);
-  });
-};*/
-
-import cloudinary from "#config/cloudinary.js";
-
 export const uploadBufferToCloudinary = (buffer, folder) => {
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
       {
         folder,
         resource_type: "image",
+        upload_preset: "vendorville_uploads",
       },
       (error, result) => {
-        if (error) {
-          console.error("Cloudinary upload error:");
-          console.error(JSON.stringify(error, null, 2));
-          return reject(error);
-        }
-
+        if (error) return reject(error);
         resolve(result.secure_url);
       },
     );
-
     stream.end(buffer);
   });
 };
