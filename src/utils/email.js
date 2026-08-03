acc6aa7a9ae53fd9794f119b92d0f08bffda2640
ikenna <agugbuenzubechi@gmail.com>
2026-08-03 20:09:20 +0100

    changed email.js from mailtrap to gmail

--- src/utils/email.js
+++ src/utils/email.js
@@ -30,7 +30,7 @@ export default class Email {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 465,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.GMAIL_USERNAME,
         pass: process.env.GMAIL_PASSWORD,
